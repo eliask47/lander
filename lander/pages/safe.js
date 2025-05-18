fdabaa7395b2928221817bca0de1d4306b687306
eliask47 <163206816+eliask47@users.noreply.github.com>
2025-05-17 22:47:35 -0400

    Delete lander directory

--- lander/pages/safe.js
+++
@@ -1,8 +0,0 @@
-export default function Safe() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>Privacy Policy</h1>
-      <p>This is a safe and compliant page for all users.</p>
-    </div>
-  );
-}
